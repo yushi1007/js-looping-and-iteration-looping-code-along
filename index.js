6bb6a36bd8ea7504f6ef962eb70dcbb46134aa5c
Yu Shi <yushiys95@gmail.com>
2021-03-14 12:55:14 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+
 // Code your solutions in this file
+function writeCards(array, event) {
+    const thankYouCards = [];
+    let i = 0;
+    while (i < array.length) {
+    thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    i++;
+    }
+    return thankYouCards;
+}
+
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    };
+}
+
+
+
+
